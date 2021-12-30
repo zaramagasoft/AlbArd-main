4e4af645fdb165936518aa2ccfd96697d7e3cd35
alb1122 oli <zaramagasoft@gmail.com>
2021-12-30 19:07:47 +0100

    otros

--- public/socketA.js
+++ public/socketA.js
@@ -32,16 +32,13 @@ socket.on("dataHard", function (data) {
 socket.on("temp", function (data) {
   //    console.log('HW->'+data)
   document.getElementById("temp").innerHTML = data;
-  
 });
 function mensajeHard(data) {
   document.getElementById("hardInf").innerHTML = data;
 }
 
 //apagar equipo etc
-function apagar(){
-    console.log('le has dao a apagar')
-    //enviar un post
-    
+function apagar() {
+  console.log("le has dao a apagar");
+  //enviar un post
 }
-
